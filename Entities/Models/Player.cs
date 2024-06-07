7978bc668fc397f25c6672edd983243ee6fc804c
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-06-07 06:35:47 -0400

    creating entities

---
+++ Entities/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+	public class Player
+	{
+		[Key]
+		[Required(ErrorMessage = "Player's name is a required field.")]
+		[MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+
+		public string? Name { get; set; }
+
+		[ForeignKey(nameof(Score))]
+		public int Score { get; set; }
+		public Score? Score { get; set; }
+	}
+}
---
+++ Entities/Models/Score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+	public class Score
+	{
+		[Column("ScoreId")]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Points is a required field.")]
+		public int Points { get; set; } = 0;
+
+		[ForeignKey(nameof(Player))]
+		public int PlayerId { get; set; }
+		public Player Player { get; set; }
+	}
+}

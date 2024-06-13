a1d0fd7932a75452afbabeed6b5c0ec4509c63c3
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-06-13 15:44:38 -0400

    Adding Roulette Models

---
+++ Entities/Models/Color.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+	public class Color
+	{
+		[Column("ColorId")]
+		public Guid Id { get; set; }
+
+		//Color black or red
+		[Required(ErrorMessage = "Color name is a required field.")]
+		public string? Name { get; set; }
+		
+		//One to Many relationship with Position - A color can have many positions
+		IEnumerable<Position>? Positions { get; set; }
+
+	}
+}
---
+++ Repository/Configuration/ColorConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuration
+{
+	public class ColorConfiguration : IEntityTypeConfiguration<Color>
+	{
+		public void Configure(EntityTypeBuilder<Color> builder)
+		{
+			builder.HasData
+				(					
+					new Color
+					{
+						Id = new Guid("21d029a4-ac35-47b7-b3ac-a380b0ea355e"),
+						Name = "red"
+					},
+					new Color
+					{
+						Id = new Guid("2da7569e-6641-45e1-9d55-44fe86338902"),
+						Name = "black"
+					}
+				);
+		}
+	}
+}
